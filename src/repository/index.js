d4cc07553f51a9388218b186abdb9803f520ded3
unknown <jhonwiww@gmail.com>
2024-05-19 17:15:22 -0500

    deployment

--- src/repository/index.js
+++ src/repository/index.js
@@ -1,6 +1,14 @@
-const UsersRepository = require('../repository/users.repository')
+/*const UsersRepository = require('../repository/users.repository')
 //const UsersMongoDao = require('../DAO/mongo/user-dao.mongo')
 const UsersFactory=require('../factory/users.factory')
 const usersRepository = new UsersRepository(new UsersFactory())
 
-module.exports = usersRepository
+module.exports = usersRepository*/
+
+// src/repository/index.js
+const UserDAO = require('../factory/users.factory');
+const UsersRepository =require('../factory/users.factory')
+
+const usersRepository = new UsersRepository(new UserDAO());
+
+module.exports = usersRepository;
